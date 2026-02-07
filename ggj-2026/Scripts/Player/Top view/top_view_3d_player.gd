670627d69780fe9c8dabd14f482866b23db90fa1
R1-Debacker <erwann@widid.fr>
2026-02-07 23:03:58 +0100

    anim intégré

--- ggj-2026/Scripts/Player/Top view/top_view_3d_player.gd
+++ ggj-2026/Scripts/Player/Top view/top_view_3d_player.gd
@@ -15,6 +15,8 @@ class_name player3D_top_view
 
 var last_direction := Vector3.FORWARD
 var sprinting := false
+var grabbing := false
+var success_grab := false
 var can_sprint := true
 var sprint_speed = default_speed * sprint_factor
 var speed
@@ -41,16 +43,17 @@ func _physics_process(delta: float):
 	move_and_slide()
 	
 	if Input.is_joy_button_pressed(device_index, JOY_BUTTON_X):
+		grabbing = true
 		var vectorToRobber = Game.fps_player.position - position
 		var distanceToRobber = vectorToRobber.length()
 		var dot = last_direction.dot(vectorToRobber)
 		anim_state.travel("Grabbing")
-		if distanceToRobber <= 5 && dot > -0.2:
+		if distanceToRobber <= 4 && dot > -0.2:
+			position = Game.fps_player.position - vectorToRobber.normalized() * 1.5
+			success_grab = true
+			Game.fps_player.target_robber = position
+			Game.fps_player.grabbed = true
 			Input.start_joy_vibration(device_index, 0.5, 0.5, 0.1) 
-			var fps_index = Game.fps_player.device_index
-			Game.fps_player.device_index = device_index
-			device_index = fps_index
-			position = Game.get_random_coord()
 		
 	
 	for i in get_slide_collision_count():
@@ -70,47 +73,50 @@ func get_move_input(delta):
 	
 	speed = default_speed
 	animation_player.speed_scale = 1.5
+	if grabbing:
+		velocity = Vector3.ZERO
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 		
-	if Input.get_joy_axis(device_index, JOY_AXIS_TRIGGER_RIGHT) > 0.9 and can_sprint and not sprinting:
-		sprinting = true
-		sprint_duration.start()
-		print("sprint start")
+	else:
+		if Input.get_joy_axis(device_index, JOY_AXIS_TRIGGER_RIGHT) > 0.9 and can_sprint and not sprinting:
+			sprinting = true
+			sprint_duration.start()
+			print("sprint start")
+			
+		if Input.get_joy_axis(device_index, JOY_AXIS_TRIGGER_RIGHT) < 0.5 and sprinting:
+			sprint_duration.stop()
+			can_sprint = false
+			sprinting = false
+			sprint_cooldown.start()
+			
+		if sprinting:
+			speed = sprint_speed
+			animation_player.speed_scale = 3.0
+			
+		var dir_z = -Input.get_joy_axis(device_index, JOY_AXIS_LEFT_Y) * Vector3.FORWARD
+		var dir_x = Input.get_joy_axis(device_index, JOY_AXIS_LEFT_X) * Vector3.RIGHT
 		
-	if Input.get_joy_axis(device_index, JOY_AXIS_TRIGGER_RIGHT) < 0.5 and sprinting:
-		sprint_duration.stop()
-		can_sprint = false
-		sprinting = false
-		sprint_cooldown.start()
+		var direction = (dir_z + dir_x)
+		var magnitude = direction.length()
+		if magnitude <= 0.3: direction = Vector3.ZERO
+		direction = direction.normalized()
 		
-	if sprinting:
-		speed = sprint_speed
-		animation_player.speed_scale = 3.0
+		anim_tree.set("parameters/IDLE-WALK-RUN/blend_position", magnitude)	
 		
-	var dir_z = -Input.get_joy_axis(device_index, JOY_AXIS_LEFT_Y) * Vector3.FORWARD
-	var dir_x = Input.get_joy_axis(device_index, JOY_AXIS_LEFT_X) * Vector3.RIGHT
-	
-	var direction = (dir_z + dir_x)
-	var magnitude = direction.length()
-	if magnitude <= 0.3: direction = Vector3.ZERO
-	direction = direction.normalized()
-	
-	anim_tree.set("parameters/IDLE-WALK-RUN/blend_position", magnitude)	
-	
-	if direction != Vector3.ZERO:
-		last_direction = direction
+		if direction != Vector3.ZERO:
+			last_direction = direction
+			
+		velocity.x = direction.x * speed * magnitude
+		velocity.z = direction.z * speed * magnitude
 		
-	velocity.x = direction.x * speed * magnitude
-	velocity.z = direction.z * speed * magnitude
-	
-	#rotation = transform.looking_at(position + direction).basis.get_euler()
-	if transform.origin != position + last_direction:
-		rotation.y = rotate_toward(rotation.y,transform.looking_at(position + last_direction).basis.get_euler().y, delta * speed_rotation)
-	
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, speed)
-		#velocity.z = move_toward(velocity.z, 0, speed)
+		#rotation = transform.looking_at(position + direction).basis.get_euler()
+		if transform.origin != position + last_direction:
+			rotation.y = rotate_toward(rotation.y,transform.looking_at(position + last_direction).basis.get_euler().y, delta * speed_rotation)
+		
+		#else:
+			#velocity.x = move_toward(velocity.x, 0, speed)
+			#velocity.z = move_toward(velocity.z, 0, speed)
 
 func _on_sprint_duration_timeout() -> void:
 	can_sprint = false
@@ -121,3 +127,16 @@ func _on_sprint_duration_timeout() -> void:
 func _on_sprint_cooldown_timeout() -> void:
 	can_sprint = true
 	print("cooldown end")
+
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Grabbing":
+		grabbing = false
+		if success_grab:
+			success_grab = false
+			var fps_index = Game.fps_player.device_index
+			Game.fps_player.device_index = device_index
+			device_index = fps_index
+			position = Game.get_random_coord()
+			Game.fps_player.rotate(Vector3.UP, 180)
+			Game.fps_player.grabbed = false
