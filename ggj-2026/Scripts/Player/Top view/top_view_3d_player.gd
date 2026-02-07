0179c6e90225254ae96c0603a4d8aaf9b6b142ca
Maxime SEIGNEUR <81438664+SeigneurM@users.noreply.github.com>
2026-02-07 15:40:31 +0100

    SPRINT PTN

--- ggj-2026/Scripts/Player/Top view/top_view_3d_player.gd
+++ ggj-2026/Scripts/Player/Top view/top_view_3d_player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 class_name player3D_top_view
 
 @export var player_index := 0
-@export var global_speed := 7.0
+@export var default_speed := 7.0
 @export var speed_rotation = 10.0
 @export var sprint_factor = 2
 #@export var acceration = 4.0
@@ -13,7 +13,8 @@ class_name player3D_top_view
 var last_direction := Vector3.ZERO
 var sprinting := false
 var can_sprint := true
-var speed = global_speed
+var sprint_speed = default_speed * sprint_factor
+var speed
 
 @onready var model := $Skeleton/Skeleton3D
 @onready var anim_tree = $AnimationTree
@@ -22,6 +23,7 @@ var speed = global_speed
 
 @onready var sprint_duration: Timer = $"Sprint duration"
 @onready var sprint_cooldown: Timer = $"Sprint cooldown"
+
 
 func _ready() -> void:
 	anim_tree.active = true
@@ -37,11 +39,25 @@ func _physics_process(delta: float):
 
 func get_move_input(delta):
 	
+	speed = default_speed
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-	
-	var sprinting = Input.is_action_pressed("top_view_sprint") and can_sprint
+		
+	if Input.is_action_just_pressed("top_view_sprint") and can_sprint and not sprinting:
+		sprinting = true
+		#if Input.is_action_just_pressed("top_view_sprint"):
+		sprint_duration.start()
+		print("sprint start")
+		
+	if Input.is_action_just_released("top_view_sprint") and sprinting:
+		sprint_duration.stop()
+		can_sprint = false
+		sprinting = false
+		sprint_cooldown.start()
+		
+	if sprinting:
+		speed = sprint_speed
+		
 	var dir_z = -Input.get_joy_axis(player_index, JOY_AXIS_LEFT_Y) * Vector3.FORWARD
 	var dir_x = Input.get_joy_axis(player_index, JOY_AXIS_LEFT_X) * Vector3.RIGHT
 	
@@ -52,14 +68,9 @@ func get_move_input(delta):
 	
 	anim_tree.set("parameters/IDLE-WALK-RUN/blend_position", magnitude)
 	
-	if sprinting:
-		animation_player.speed_scale = 2.0
-		if Input.is_action_just_pressed("top_view_sprint"):
-			sprint_duration.start()
-		speed = global_speed * sprint_factor
-	else:
-		speed = global_speed
-		animation_player.speed_scale = 1.5
+	#animation_player.speed_scale = 1.5
+	#animation_player.speed_scale = 2.0
+		
 	
 	if direction != Vector3.ZERO:
 		last_direction = direction
@@ -68,22 +79,21 @@ func get_move_input(delta):
 	velocity.z = direction.z * speed * magnitude
 	
 	#rotation = transform.looking_at(position + direction).basis.get_euler()
-	if rotation.y != transform.looking_at(position + last_direction).basis.get_euler().y:
+	if transform.origin != position + last_direction:
 		rotation.y = rotate_toward(rotation.y,transform.looking_at(position + last_direction).basis.get_euler().y, delta * speed_rotation)
 	
 	#else:
 		#velocity.x = move_toward(velocity.x, 0, speed)
 		#velocity.z = move_toward(velocity.z, 0, speed)
 
-	if Input.is_action_just_released("top_view_sprint") and can_sprint:
-		sprint_duration.stop()
-		can_sprint = false
-		sprint_cooldown.start()
 
 func _on_sprint_duration_timeout() -> void:
 	can_sprint = false
+	sprinting = false
+	print("not sprint anymore")
 	sprint_cooldown.start()
 
 
 func _on_sprint_cooldown_timeout() -> void:
 	can_sprint = true
+	print("cooldown end")
