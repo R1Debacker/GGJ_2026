0929ad86b1d5c54be832c537a06b6f07ed785469
Maxime SEIGNEUR <81438664+SeigneurM@users.noreply.github.com>
2026-02-07 20:31:05 +0100

    random mieux wesh

--- ggj-2026/autoloads/Game.gd
+++ ggj-2026/autoloads/Game.gd
@@ -31,11 +31,13 @@ func get_random_coord() -> Vector3:
 	
 	var ran_coord = Vector3(0,0,0)
 	
-	ran_coord.x = randi() % 97
-	ran_coord.z = randi() % 55
-	while ran_coord.x in [32,64] and ran_coord.z in [18,36]:
-		ran_coord.x = randi() % 97
-		ran_coord.z = randi() % 55
+	ran_coord.x = randi_range(-47, 47)
+	ran_coord.z = randi_range(-26, 26)
+
+	while ran_coord.x in range(-15, 15) and ran_coord.z in range(-8, 8):
+		
+		ran_coord.x = randi_range(-47, 47)
+		ran_coord.z = randi_range(-26, 26)
 			
 	return ran_coord
 	
