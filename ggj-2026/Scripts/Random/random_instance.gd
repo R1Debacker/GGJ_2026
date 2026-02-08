e10d4ff952b0664737801bb7d1d446f5994cc868
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2026-02-08 13:47:07 +0100

    feat: add filpable Z on room + fix some artroom colider box

--- ggj-2026/Scripts/Random/random_instance.gd
+++ ggj-2026/Scripts/Random/random_instance.gd
@@ -2,14 +2,26 @@ extends Node3D
 class_name RandomInstance
 
 @export var rotatable := true
+@export var flipable_z := true
 
 func instanciate(scene_path):
 	var packed := load(scene_path) as PackedScene
+	if not packed:
+		return
 	
-	if packed:
-		var inst : Node3D = packed.instantiate()
-		add_child(inst)
+	var inst : Node3D = packed.instantiate()
+	add_child(inst)
 	
 	if self.rotatable and randi() % 2 == 0:
 		# Rotate
 		rotate(Vector3.UP, PI)
+	
+	if self.flipable_z and randi() % 2 == 0:
+		# Flip Z
+		var lights = inst.get_node_or_null("Lights")
+		if not lights:
+			print("Room " + scene_path + " has no Lights node")
+			return
+		
+		lights.reparent(self)
+		inst.scale.z = -1
